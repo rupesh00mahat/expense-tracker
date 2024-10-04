a48561118414dad524cc0fe55eead46d3534d41f
rupesh00mahat <rupesh00mahat@gmail.com>
2024-10-04 06:12:17 +0545

    complete expense tracker

--- app/components/container-category-item.jsx
+++ app/components/container-category-item.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-function ContainerCategoryItem({value,type}) {
+function ContainerCategoryItem({value,type, total}) {
   return (
     <div className="category-container-item container border-2 border-solid border-black-500 p-5 rounded-md my-10">
       <span className="category-item-income text-3xl">{type} by Category</span>
       {value && value.map(({category, amount}, index)=>{
+          const progressValue = parseInt(parseInt(amount)/parseInt(total)*100);
+
         return <div key={`${index}`} className="category-items text-xl flex justify-between">
         <div className="category-item-right w-full">
           <div className="info flex items-center justify-between">
             <span className="category-name">{category} </span>
             <span className="percentage-amount">{amount}</span>
           </div>
-          <div className="progress-loader w-full h-2 border border-red-400 rounded-md relative after:absolute after:h-full after:w-[40%] after:bg-blue-500"></div>
+          <div style={{"--progress-width": `${progressValue}%`}} className={`progress-loader w-full h-2 border border-red-400 rounded-md relative after:absolute after:h-full after:w-[${progressValue}%] after:bg-blue-500`}></div>
         </div>
       </div>
       })}
@@ -20,3 +22,4 @@ function ContainerCategoryItem({value,type}) {
 }
 
 export default ContainerCategoryItem;
+
--- app/components/overview.jsx
+++ app/components/overview.jsx
@@ -1,12 +1,30 @@
 "use client";
-import React, { useContext, useRef } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import ContainerItem from "./container-item";
 import ContainerCategoryItem from "./container-category-item";
 import { ExpenseTrackerContext } from "../context/expense-tracker-context";
+import { PieChart } from '@mui/x-charts/PieChart';
 
 function Overview() {
   const { state, dispatch } = useContext(ExpenseTrackerContext);
-  console.log(state);
+
+    const [totalIncome, setTotalIncome] = useState(0);
+    const [totalExpense, setTotalExpense] = useState(0);
+
+    useEffect(()=>{
+      let incomeSum = 0;
+      let expenseSum = 0;
+      state.income.forEach((item)=>{
+        incomeSum += parseInt(item.amount);
+      })
+      state.expense.forEach((item)=>{
+        expenseSum += parseInt(item.amount);
+      })
+     
+      setTotalIncome(incomeSum);
+      setTotalExpense(expenseSum);
+    },[state.income, state.expense])
+
   return (
     <div id="overview">
       <h2 className="text-center text-3xl mb-10">Overview</h2>
@@ -15,9 +33,21 @@ function Overview() {
         <ContainerItem title="Expense" value={state.expense} />
       </div>
       <div className="category-container-wrapper flex gap-20 justify-between">
-        <ContainerCategoryItem value={state.income} type="Income"/>
-        <ContainerCategoryItem value={state.expense} type="Expense"/>
+        <ContainerCategoryItem value={state.income} type="Income" total={totalIncome}/>
+        <ContainerCategoryItem value={state.expense} type="Expense" total={totalExpense}/>
       </div>
+      <PieChart
+  series={[
+    {
+      data: [
+        { id: 0, value: totalIncome, label: 'Income', color: "blue" },
+        { id: 1, value: totalExpense, label: 'Expense', color: "red" },
+      ],
+    },
+  ]}
+  width={400}
+  height={200}
+/>
     </div>
   );
 }
